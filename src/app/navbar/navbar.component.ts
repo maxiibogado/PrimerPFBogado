1892bcb89420d93e53ea7f252aae612419ae637e
Maxi <mbogado@xubio.com>
2023-04-17 08:29:56 -0300

    Subimos ultimo cambio

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
-import { FullNamePipe } from './full-name.pipe';
 
 
 @Component({
